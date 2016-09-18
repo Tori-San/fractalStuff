52286f97660bce74bca3100c706e5da9bbf6e2ca
Kai Vogelgesang <s8kavoge@stud.uni-saarland.de>
2016-09-18 13:59:35 +0200

    Bar now prints percentage, added a start() method

--- utils/pr.py
+++ utils/pr.py
@@ -6,16 +6,23 @@ class Bar:
         self.full = full
         self.empty = empty
         self.length = length
-        self.totalLength = len(left) + length + len(right)
+        self.totalLength = len(left) + length + len(right) + 7
         self.progress = 0
+    
+    def start(self):
+        print(self.left + self.length * self.empty + self.right, end="")
 
     def update(self, x):
         progress = int(self.length * x + 0.5)
         if progress > self.progress:
             self.progress = progress
             print("\r" * self.totalLength, end="")
-            print(self.left + self.progress * self.full + (self.length - self.progress) * self.empty + self.right, end="")
+            print(self.left +
+                  self.progress * self.full +
+                  (self.length - self.progress) * self.empty +
+                  self.right +
+                  " {:.2f}%".format(100*x).rjust(6), end="")
 
     def end(self):
         print("\r" * self.totalLength, end="")
-        print(self.left + self.length * self.full + self.right)
+        print(self.left + self.length * self.full + self.right + 7*" ")
